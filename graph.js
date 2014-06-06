c0c2055479ae732f2dec69abfe64445af2b06f45
Steven Schlansker <sschlansker@opentable.com>
2014-06-06 14:21:52 -0700

    Dumb comments to transfer home

--- graph.js
+++ graph.js
@@ -1,3 +1,11 @@
 (function() {
   google.setOnLoadCallback(function() { alert("hi!"); });
 })();
+
+
+//   params = urlencode({
+//    'q': '+applicationName:%s +level:request' % (name),
+//    'size': 1000000
+//  })
+
+// 'http://sc-logsearch-01.otsql.opentable.com:9200/logstash-2014.06.05/_search?%s' % param
